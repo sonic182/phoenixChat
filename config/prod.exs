39a6355f484a025e42dbf4cef28c2f6ac623745d
Johanderson <johander1822@gmail.com>
2016-09-12 22:01:51 +0200

    check hostname fix

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :phoenixChat, PhoenixChat.Endpoint,
   http: [port: {:system, "PORT"}],
   # url: [host: "example.com", port: 80],
-  url: [scheme: "https", host: "glacial-hollows-78730.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "warm-meadow-51920.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
